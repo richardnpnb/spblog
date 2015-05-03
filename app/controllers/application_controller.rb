ae676d17f29b3a878ea55421e0d6ed2e7eeb3483
Richard Noble <richardnpnb@users.noreply.github.com>
2015-05-03 11:45:06 +0100

    Revert "Test Branch"
    This reverts commit 4e923155710fab140f9c60584bb8f8231c643962.
    
    Conflicts:
    	app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
 <<<<<<< HEAD
+<<<<<<< HEAD
   # Prevent CSRF attacks by raising an exception.
 =======
-  # Prevent CSRF attacks by raising an exception.  TEST BRANCH
+  # Prevent CSRF attacks by raising an exception.  TEST DIDN'T WORK
 >>>>>>> TestBranch
+=======
+  # Prevent CSRF attacks by raising an exception.  
+>>>>>>> parent of 4e92315... Test Branch
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
